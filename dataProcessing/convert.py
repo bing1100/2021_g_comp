4c7d126c94dc15d67597d2ed6f5d9bc8d2c5b9a5
bing1100 <bingxuhu@gmail.com>
2021-02-18 13:44:54 -0500

    cleaned up code

--- dataProcessing/convert.py
+++ dataProcessing/convert.py
@@ -5,12 +5,12 @@ from PIL import Image
 from numpy import asarray
 import json
 
-# Note: Copy this file to the train directory before running. s
+# Fill these parameters
 NUMPROCESS = 8
 NEWWIDTH = 150
 NEWHEIGHT = 150
 STRIDE = 50
-CREATE = True
+CREATE = True   # Mark true or false if you want to create images
 PATH = "/media/bhux/ssd/grss_dse"
 
 train = PATH + "/dfc2021_dse_train/Train"
@@ -64,7 +64,6 @@ for tile in range(1,20):
     for y in range(16):
         for x in range(16):
             pts = findSurround(y, x)
-    
             chunkStartPt = (pts[0][0] * 50, pts[0][1] * 50)
             
             data["Tile"+str(tile)+"_"+str(y)+"_"+str(x)] = {
@@ -74,6 +73,8 @@ for tile in range(1,20):
     with open(val + 'Tile' + str(tile) + '.txt', 'w') as outfile:
         json.dump(data, outfile)
         
+        
+# Created image chunks for specific tile 
 def processImg(tileNum,type):
     folder = PATH + type + "/" + "Tile" + str(tileNum)
     path = PATH + "/chunked" + type
@@ -87,27 +88,26 @@ def processImg(tileNum,type):
             if '.tif' in file and 'groundTruth' not in file:
                 files.append((os.path.join(r,file), file))
 
+    # Make all of the folders for the tile
     for y in range(16):
         for x in range(16):
             os.makedirs(path + "/Tile"+str(tileNum)+"_"+str(y)+"_"+str(x),exist_ok=True)
     
+    # Generate the new chunks
     for file in files:
-
-        img = cv2.imread(file[0], -1)
         
+        # Load image with 50px padding
+        img = cv2.imread(file[0], -1)
         img = cv2.copyMakeBorder(img, 50, 50, 50, 50, cv2.BORDER_CONSTANT)
+        
         for y in range(16):
             for x in range(16):
-                pts = findSurround(y, x)
-                i = (pts[0][0] * 50, pts[0][1] * 50)
+                i = (y * 50, x * 50)
                 
                 newImgPath = path + "/Tile"+str(tileNum)+"_"+str(y)+"_"+str(x) +"/" + file[1]
-                
                 split = img[i[0]:i[0]+NEWHEIGHT, i[1]:i[1]+NEWWIDTH]
                 cv2.imwrite(newImgPath, split)
-        
-        
-                       
+                    
 if CREATE:
     p = Pool(NUMPROCESS)
     results = p.map(lambda a : processImg(a, "/dfc2021_dse_train/Train"), list(range(1,61)))
